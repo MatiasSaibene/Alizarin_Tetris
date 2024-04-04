bddfe87dadf2aa1c62a6df63530ed0114342a6e6
MatiasSaibene <matiassaibene98@gmail.com>
2024-04-04 12:42:03 -0300

    First commit

---
+++ fastrand.h
@@ -0,0 +1,8 @@
+
+/* Declarations for the fast random functions */
+
+#include <SDL/SDL_stdinc.h>
+extern void   SeedRandom(Uint32 seed);
+extern Uint16 FastRandom(Uint16 range);
+extern Uint32 GetRandSeed(void);
+
